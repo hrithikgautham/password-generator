043634fb4f0ecc526e3856525a8e5199c7d4c06c
hrithik <htg2ana@gmail.com>
2019-12-23 16:48:11 +0530

    something wrong with display input element

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 /* variables */
-const rangeInput = document.querySelector('#number-of-chars');
+const displayInput = document.querySelector('.display input');
 const clipBoard = document.querySelector('.clip-board');
+const passwordChars = document.querySelector('#number-of-chars');
+const includeLowercase = document.querySelector('#include-lowercase');
+const includeUppercase = document.querySelector('#include-uppercase');
+const includeNumbers = document.querySelector('#include-numbers');
+const includeSymbols = document.querySelector('#include-symbols');
 const randomFunc = {
     lower: getRandomLowerCase,
     upper: getRandomUpperCase,
@@ -9,13 +14,38 @@ const randomFunc = {
 }
 
 /* event listeners */
-rangeInput.addEventListener('click', highlightRange);
+passwordChars.addEventListener('click', highlightRange);
 clipBoard.addEventListener('click', copyToClipBoard);
 // clipBoard.addEventListener('mouseOver', selectInputText)
+// generateButton.addEventListener('click', handleGenerateButtonClick);
+document.forms[0].addEventListener('submit', handleGenerateButtonClick);
+displayInput.addEventListener('change', (e) => {
+    return;
+});
 
 /* event handlers */
 function highlightRange(e) {
-    rangeInput.select();
+    passwordChars.select();
+}
+
+function handleGenerateButtonClick(e) {
+    e.preventDefault();
+    const includeLower = includeLowercase.checked;
+    const includeUpper = includeUppercase.checked;
+    const includeSym = includeSymbols.checked;
+    const includeNum = includeNumbers.checked;
+    if(!includeLower && !includeNum && !includeSym && !includeUpper){
+        console.log("atleast on checkbox should be checked");
+        return;
+    }
+    console.log(document.querySelector('.display input').value);
+    displayInput.value = generatePassword(
+        passwordChars.value,
+        includeLower,
+        includeUpper,
+        includeNum,
+        includeSym
+    );
 }
 
 // function selectInputText(e) {
@@ -29,6 +59,16 @@ function copyToClipBoard(e) {
 
 
 /* functions */
+function generatePassword(numChars, lower, upper, number, symbol) {
+    let password = [];
+    let arr = [{ lower }, { upper }, { number }, { symbol }].filter(ele => Object.values(ele)[0]);
+    for(let i = 0 ; i < numChars ; i++) {
+        const funcName = Object.keys(arr[Math.floor(Math.random() * arr.length)])[0];
+        password.push(randomFunc[funcName]());
+    }
+    return password.join("");
+}
+
 function getRandomLowerCase() {
     return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
 }
@@ -47,3 +87,4 @@ function getRandomSymbol() {
 }
 
 // console.log(getRandomLowerCase());
+
